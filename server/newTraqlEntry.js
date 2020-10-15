4a216712ec6c6655f521b4fc55060e249d85f199
Case Simmons <casesimmons@pm.me>
2020-10-14 21:03:36 -0700

    (update) newTraqlEntry to create userToken on each Traql entry
    Co-authored-by Michael O’Halloran <45404541+LordRegis22@users.noreply.github.com>

--- server/newTraqlEntry.js
+++ server/newTraqlEntry.js
@@ -5,9 +5,11 @@ function newTraqlEntry(traql, args, pubsub) {
   traql[args.aql.mutationId] = {
     resolver: args.aql.resolver,
     openedTime: Date.now(),
-    expectedNumberOfAqls: Object.keys(pubsub.subscriptions).length / traql.subResolvers,
+    expectedNumberOfAqls: Math.floor(
+      Object.keys(pubsub.subscriptions).length / traql.subResolvers
+    ),
     aqlsReceivedBack: [],
-    userToken: args.aql.userToken,
+    userToken: traql.userToken,
   };
 }
 
