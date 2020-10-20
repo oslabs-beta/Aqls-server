fcdc6d96d0c267ca43dea2e8f1fe94459884246f
LordRegis22 <45404541+LordRegis22@users.noreply.github.com>
2020-10-20 15:06:56 -0700

    Add endpoint for timesyncServer

--- server/analyticsRouter.js
+++ server/analyticsRouter.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const timesyncServer = require('timesync/server');
+
+/* Looks for mutation ID in the traql object and pushes the number
+    of aqls received back pertaining to its mutation ID */
 
 function analyticsRouter(traql) {
-
-  return router.post(
-  '/',
-  (req, res, next) => {
-
-    //Looks for mutation ID in the traql object 
-      //pushes the number of aqls received back pertaining to its mutation ID
-    traql[req.body.mutationId].aqlsReceivedBack.push(req.body);
-    return next();
-  },
-  (req, res) => {
-    res.sendStatus(200);
-  }
- );
-};
+  router.post(
+    '/',
+    (req, res, next) => {
+      traql[req.body.mutationId].aqlsReceivedBack.push(req.body);
+      return next();
+    },
+    (req, res) => {
+      res.sendStatus(200);
+    }
+  );
+  router.post('/timesync', timesyncServer.requestHandler);
+  return router;
+}
 
 module.exports = analyticsRouter;
